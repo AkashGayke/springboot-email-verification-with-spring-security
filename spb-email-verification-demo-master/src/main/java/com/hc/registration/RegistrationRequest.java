53bd6d1e3278a9ea4ad3e5f2b2a6afc4bdd809d3
Akash Gayke <akash.gayke@humancloud.co.in>
2023-05-23 14:48:26 +0530

    first commit

---
+++ spb-email-verification-demo-master/src/main/java/com/hc/registration/RegistrationRequest.java
@@ -0,0 +1,9 @@
+package com.hc.registration;
+
+public record RegistrationRequest(
+         String firstName,
+         String lastName,
+         String email,
+         String password,
+         String role) {
+}
